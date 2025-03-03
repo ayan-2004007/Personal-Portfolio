09b881985361c488804bea6b3e0d925b1410c17a
Ayan Chakraborty <ayanchakraborty@gmail.com>
2025-03-03 18:10:45 +0530

    Added Smooth scrolling

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import Navbar from '../Components/Navbar'
 import HomeHero from "../Components/HomeHero"
+
+gsap.registerPlugin(ScrollTrigger);
 const Home=()=>{
+    useEffect(()=>{
+        const lenis=new Lenis({
+            duration: 1,
+            easing: (t) =>  t * (2 - t),
+            smoothTouch: false,
+            inertia: 0.95,
+            syncTouch: true,
+        })
+
+        function raf(time) {
+            lenis.raf(time);
+            ScrollTrigger.update();
+            requestAnimationFrame(raf);
+          }
+          requestAnimationFrame(raf);
+          return () => {
+            lenis.destroy();
+          };
+    },[])
     return(
             <body>
                 {/* <Navbar/> */}
                 <HomeHero/>
+                <div className="w-full h-screen bg-red-200"></div>
+                <div className="w-full h-screen bg-green-200"></div>
+
             </body>
     )
 }
